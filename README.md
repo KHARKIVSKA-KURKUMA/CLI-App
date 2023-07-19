5812e31a27e7565a1bf314adce512dde8bd9dc13
Vasiuk Kateryna <catherine.vasyuk11@gmail.com>
2023-07-19 21:13:10 +0300

    final version

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-Hello there
+# Отримуємо і виводимо весь список контактів у вигляді таблиці (console.table)
+
+node index.js --action="list"
+![list](./assets/list.png)
+
+# Отримуємо контакт по id і виводимо у консоль об'єкт контакту або null, якщо контакту з таким id не існує.
+
+node index.js --action="get" --id 1
+![get](./assets/get.png)
+
+# Додаємо контакт та виводимо в консоль об'єкт новоствореного контакту
+
+node index.js --action="add" --name Mango --email mango@gmail.com --phone 322-22-22
+![add](./assets/add.png)
+
+# Видаляємо контакт та виводимо в консоль об'єкт видаленого контакту або null, якщо контакту з таким id не існує.
+
+node index.js --action="remove" --id 1
+![remove](./assets/remove.png)
--- index.js
+++ index.js
@@ -1,23 +1,45 @@
-// contacts.js
+const chalk = require("chalk");
+const contacts = require("./db/contacts");
 
-/*
- * Розкоментуй і запиши значення
- * const contactsPath = ;
- */
+const { Command } = require("commander");
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
 
-// TODO: задокументувати кожну функцію
-function listContacts() {
-  // ...твій код. Повертає масив контактів.
-}
+program.parse(process.argv);
 
-function getContactById(contactId) {
-  // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
-}
+const argv = program.opts();
 
-function removeContact(contactId) {
-  // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
-}
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    /* ---------------------------------- LIST ---------------------------------- */
+    case "list":
+      const allContacts = await contacts.listContacts();
+      console.table(allContacts);
+      break;
+    /* ----------------------------------- GET ---------------------------------- */
+    case "get":
+      const getContact = await contacts.getContactById(id);
+      console.log(getContact);
+      break;
+    /* ----------------------------------- ADD ---------------------------------- */
+    case "add":
+      const newContact = await contacts.addContact(name, email, phone);
+      console.log(newContact);
+      break;
+    /* --------------------------------- REMOVE --------------------------------- */
+    case "remove":
+      const removeContact = await contacts.removeContact(id);
+      console.log(removeContact);
+      break;
+    /* --------------------------------- DEFAULT -------------------------------- */
+    default:
+      console.log(chalk.bold.bgRed(" Unknown action type!"));
+  }
+};
 
-function addContact(name, email, phone) {
-  // ...твій код. Повертає об'єкт доданого контакту.
-}
+invokeAction(argv);
